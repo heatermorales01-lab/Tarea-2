142084dbf88beda0a4fe428fe0aef177f354a14a
Heather <heatermorales01@gmail.com>
2025-09-27 17:22:09 -0600

    Tarea 2 Biblioteca POO

---
+++ Libro.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea2
+{
+    internal class Libro
+    {
+        //Atributos
+        public string Titulo;
+        public string Autor;
+        public int Ano;
+        
+
+        //Constructor
+        public Libro(string titulo, string autor, int ano)
+        {
+            Titulo = titulo;
+            Autor = autor;
+            Ano = Math.Abs(ano);
+        }
+
+        //Método
+        public void MostrarInformacion()
+        {
+            Console.WriteLine($"{Titulo}");
+            Console.WriteLine($"Autor: {Autor}");
+            Console.WriteLine($"Año: {Ano}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Tarea2;
+
+Console.WriteLine(" ____  __ ____  __    __   ___   ______  ____   ___  ___ \r\n || )) || || )) ||    ||  // \\\\  | || | ||     //   // \\\\\r\n ||=)  || ||=)  ||    || ((   ))   ||   ||==  ((    ||=||\r\n ||_)) || ||_)) ||__| ||  \\\\_//    ||   ||___  \\\\__ || ||\r\n                                                         \n");
+Console.WriteLine("Libros disponibles: \n");
+
+List<Libro> libros = new List<Libro>
+{
+   new Libro("Cien años de soledad", "Gabriel García Márquez", 1967),
+    new Libro("El código Da Vinci", "Dan Brown", 2003),
+    new Libro("Don Quijote de la Mancha", "Miguel de Cervantes", 1605),
+    new Libro("Orgullo y prejuicio", "Jane Austen", 1813),
+    new Libro("1984", "George Orwell", 1949)
+};
+
+foreach (Libro libro in libros)
+{
+    libro.MostrarInformacion();
+    Console.WriteLine("--");
+}
